3d48321b9ae072f191557ca1500c2996b082cb70
Aditya Surana <aditya.surana@proshore.eu>
2023-05-22 16:46:31 +0545

    tweets added

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-from .models import Profile
+from .models import Profile, Tweet
 
 # Register your models here.
 admin.site.unregister(Group)
@@ -20,3 +20,4 @@ admin.site.unregister(User)
 
 admin.site.register(User,UserAdmin)
 # admin.site.register(Profile)
+admin.site.register(Tweet)
--- tweet/views.py
+++ tweet/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .models import Profile
+from .models import Profile, Tweet
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{})
+    if request.user.is_authenticated:
+        tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request,'home.html',{'tweets':tweets})
 
 def profile_list(request):
     if request.user.is_authenticated:
@@ -17,6 +19,7 @@ def profile_list(request):
 def profile(request,pk):
     if request.user.is_authenticated:
         profiles = Profile.objects.get(user__id=pk)
+        tweets = Tweet.objects.filter(user__id=pk)
         if request.method == "POST":
             
             current_user_profile = request.user.profile
@@ -28,7 +31,7 @@ def profile(request,pk):
             else:
                 print("something went wrong")
             current_user_profile.save()
-        return render(request,'tweet/profile.html',{'profiles':profiles})
+        return render(request,'tweet/profile.html',{'profiles':profiles,'tweets':tweets})
     else:
         messages.warning(request,"You must be loggedin to continue...")
         return redirect('home')
